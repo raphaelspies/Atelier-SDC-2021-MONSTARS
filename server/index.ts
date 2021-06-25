3ac3c7de452341d1ee845249e1be924bb22303a0
Nick Kim <nicholaskim101@yahoo.com>
2021-06-25 12:26:23 -0700

    'Initial api contrucstion and starting on data maniuplation for getreviews'

--- server/index.ts
+++ server/index.ts
@@ -9,15 +9,22 @@ const db: Knex = require('./db/');
 
 const QuestionsAndAnswers = require('./qa/service');
 
+const Reviews = require('./reviews/service');
+
 async function decorateFastifyInstance(): Promise<void> {
   const questionsAndAnswers = new QuestionsAndAnswers(db);
+
+  const reviews = new Reviews(db);
   fastify.decorate('qna', questionsAndAnswers);
+
+  fastify.decorate('reviews', reviews);
 }
 
 fastify
   .register(fp(decorateFastifyInstance))
   .register(require('./products'), { prefix: '/products' })
-  .register(require('./qa'), { prefix: '/qa' });
+  .register(require('./qa'), { prefix: '/qa' })
+  .register(require('./reviews'), { prefix: '/reviews' });
 
 fastify.listen(3000)
   .then((address: string) => console.log('listening on ', address))
